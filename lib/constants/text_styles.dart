0fb4ada01a522af61407d5208c1f3d578a6186f8
Ahmed Tohamy <atohamy@egypt.itgsolutions.com>
2022-06-26 17:14:55 +0200

    Added splash screen

--- lib/constants/text_styles.dart
+++ lib/constants/text_styles.dart
@@ -1,1 +1,7 @@
 part of 'constants.dart';
+
+final TextStyle kWhiteBigBoldTextStyle = GoogleFonts.poppins(
+  color: Colors.white,
+  fontSize: 60,
+  fontWeight: FontWeight.bold,
+);
--- lib/features/splash/presentation/pages/splash_page.dart
+++ lib/features/splash/presentation/pages/splash_page.dart
@@ -1,10 +1,37 @@
+import 'package:actors_pedia/constants/constants.dart';
+import 'package:actors_pedia/core/helpers/app_navigator.dart';
+import 'package:actors_pedia/features/home/presentation/pages/home_page.dart';
 import 'package:flutter/material.dart';
 
-class SplashPage extends StatelessWidget {
+class SplashPage extends StatefulWidget {
   const SplashPage({Key? key}) : super(key: key);
 
   @override
+  State<SplashPage> createState() => _SplashPageState();
+}
+
+class _SplashPageState extends State<SplashPage> {
+  @override
+  void initState() {
+    super.initState();
+    Future.delayed(const Duration(seconds: 3)).then(
+      (_) => AppNavigator.pushReplacement(
+        context,
+        widget: const HomePage(),
+      ),
+    );
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      backgroundColor: Theme.of(context).primaryColor,
+      body: Center(
+        child: Text(
+          kAppTitleString.toUpperCase(),
+          style: kWhiteBigBoldTextStyle,
+        ),
+      ),
+    );
   }
 }
